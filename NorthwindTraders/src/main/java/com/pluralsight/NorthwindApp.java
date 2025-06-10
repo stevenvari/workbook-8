b2f2d95d4621105d585403c53efbceddf60d92bf
Steven Vargas <stevenvari13@gmail.com>
2025-06-10 10:27:25 -0400

    exercise 2 in progress

--- NorthwindTraders/src/main/java/com/pluralsight/NorthwindApp.java
+++ NorthwindTraders/src/main/java/com/pluralsight/NorthwindApp.java
@@ -7,18 +7,26 @@ public class NorthwindApp {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         // load the MySQL Driver, next line no longer needed in newer versions of Java
 //         Class.forName("com.mysql.cj.jdbc.Driver");
+        if (args.length != 2) {
+            System.out.println(
+                    "Application needs two arguments to run: " +
+                            "java com.pluralsight.UsingDriverManager <username> <password>");
+            System.exit(1);
+        }
 
+        String username = args[0];
+        String password = args[1];
         // 1. open a connection to the database
         // use the database URL to point to the correct database
         Connection connection;
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "password");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", username, password);
 
         // create statement
         // the statement is tied to the open connection
         Statement statement = connection.createStatement();
 
         // define your query
-        String query = "SELECT * FROM products ";
+        String query = "SELECT * FROM products";
         System.out.println(query);
         // 2. Execute your query
         ResultSet results = statement.executeQuery(query);
